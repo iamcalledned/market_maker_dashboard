3d1653f1b540bc62dcdf8a891c1441b1df0e2381
Ned <iamcalledned@gmail.com>
2025-04-14 16:09:07 -0400

    Update app.py

--- market_dashboard/app.py
+++ market_dashboard/app.py
@@ -416,4 +416,4 @@ def get_composite_score():
 
 if __name__ == "__main__":
     start_background_updaters()
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000, debug=True)
